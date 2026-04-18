0629d80d206f2ec6705cf5ca41fa570eae3cfccb
Seo-young Silvia Kim <sy.silvia.kim@gmail.com>
2026-04-18 13:11:10 +0900

    live coding update

--- stm.R
+++ stm.R
@@ -4,10 +4,18 @@ library(quanteda)
 library(stm)
 library(here)
 library(KoNLP)
+library(tidytext)
 
 # Helpers ======================================================================
 particle_suffixes <- c(
-  "과", "와", "은", "는", "이", "가", "을", "를"
+  "과", "와", "은", "는", "이", "가", "을", "를", "의",
+  "하고", "하겠", "읍니다", "하여야", "있읍니다", "있읍니",
+  "께서", "도", "으냐", "그러나", "드리고", "한다고", "하면",
+  "주시지", "주세요", "으로", "으로써", "에나", "읍니다만", 
+  "우리", "여러분", "것입니다", "것입니", "대해서",
+  "특히", "통해서", "중에", "에라도", "이런", "합니다", "말씀드립니다마",
+  "등등", "간에", "어떻게", "그렇다면", "것만이라도", "그러한",
+  "칩니다", "주십시오", "빕니다", "이였으나"
 )
 
 extract_metadata <- function(path) {
@@ -48,6 +56,7 @@ pdf_list <- list.files(
 )
 
 corpus <- pdf_list %>%
+  ## purrr::
   map_dfr(extract_metadata) %>%
   mutate(
     text = map_chr(path, ~ paste(pdf_text(.x), collapse = "\n")),
@@ -56,3 +65,56 @@ corpus <- pdf_list %>%
 
 # Document-term matrix (DTM) ===================================================
 ## Sometimes called DFM (document-feature matrix)
+tokens_assembly <- quanteda::tokens(corpus$tokens)
+class(tokens_assembly)
+
+dfm_all <- dfm(tokens_assembly)
+dim(dfm_all)
+
+## sparsity problem을 어떻게든 하기 위해서
+dfm_trimmed <- dfm(tokens_assembly) %>%
+  dfm_trim(min_docfreq = 2, min_termfreq = 5) 
+dim(dfm_trimmed)
+
+# TF-IDF =======================================================================
+## (Cleaning X)
+tfidf_assembly <- corpus %>%
+  unnest_tokens(word, text) %>%
+  group_by(file) %>%
+  count(word, sort = TRUE) %>%
+  bind_tf_idf(word, file, n) %>%
+  arrange(desc(tf_idf))
+
+# Structural topic modeling ====================================================
+stm_assembly <- convert(
+  dfm_trimmed,
+  to = "stm",
+  docvars = corpus %>% select(date, post_april19, file)
+)
+
+## How to set K?
+k_grid <- 3:7
+search_assembly <- searchK(
+  documents = stm_assembly$documents,
+  vocab = stm_assembly$vocab,
+  K = k_grid,
+  prevalence = ~ post_april19,
+  data = stm_assembly$meta,
+  init.type = "Spectral",
+  seed = 1234
+)
+
+View(search_assembly$results)
+final_assembly <- searchK(
+  documents = stm_assembly$documents,
+  vocab = stm_assembly$vocab,
+  K = 5,
+  prevalence = ~ post_april19,
+  data = stm_assembly$meta,
+  init.type = "Spectral",
+  seed = 1234
+)
+
+
+
+
